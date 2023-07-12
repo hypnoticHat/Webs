1bd8c45853b5cb029bfac5f135f55e6f9fe6c9da
hypnoticHat <nnlbmt123@gmail.com>
2023-07-12 13:41:43 +0700

    first connect

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'mycluster.fsbak1a.mongodb.net',
+    DB_USER: 'userAdmin',
+    DB_PASS: '0copass',
+    DB_DATABASE: 'shoppingonline',
+    EMAIL_USER: 'nnlbmt123@gmail.com', // Microsoft mail service
+    EMAIL_PASS: 'jgpzbxfgpkgazadw',
+    JWT_SECRET: '<jwt_secret>',
+    JWT_EXPIRES: '100000000', // in milliseconds
+  };
+  module.exports = MyConstants;
